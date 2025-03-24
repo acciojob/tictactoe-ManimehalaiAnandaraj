88943598943089206e02be38caf2687d328fb2d1
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-24 11:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const submitbtn=document.getElementById("submit");
+const player1input=document.getElementById("player-1");
+const player2input=document.getElementById("player-2");
+const messagediv=document.querySelectorAll('.cell');
+let currentplayer="X";
+let player1="";
+let player2="";
+let gameboard=["","","","","","","","",""];
+let gameover=false;
+
+submitbtn.addEventListener("click",() =>{
+	player1=player1input.value;
+	player2=player2input.value;
+	if(player1 && player2)
+	{
+		messagediv.textContent=`${player1},you're up`;
+		player1input.disabled=true;
+		player2input.disabled=true;
+		submitbtn.disabled=true;
+	}
+	else
+	{
+		messagediv.textContent="Enter names:";
+	}
+});
+
+cells.forEach(cell =>{
+	cell.addEventListener("click",() =>{
+	if(!player1 || !player2 || gameover || cell.textContent !== "")
+		return;
+		const cellid=parseInt(cell.id)-1;
+		gameboard[cellid]=currentplayer;
+		cell.textContent=currentplayer;
+
+		if(checkWin())
+		{
+			messagediv.textContent=`${currentplayer === "X" ? player1:player2}
+			congrats you won!`;
+			gameover true;
+			return;
+		}
+	})
+})
